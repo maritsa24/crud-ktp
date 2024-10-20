52adeb342b0a175795c43ef1728507c39bb24d27
maritsa24 <102478664+maritsa24@users.noreply.github.com>
2024-10-20 22:03:36 +0700

    test

--- database/migrations/2024_10_19_193947_create_user_activities_table.php
+++ database/migrations/2024_10_19_193947_create_user_activities_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('activity');
             $table->string('url')->nullable();
             $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
         });
     }
 
